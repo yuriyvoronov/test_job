d22fd68d9e13d08785144afe57587fb5f1595350
Yuriy Voronov <voronov92@gmail.com>
2018-05-24 02:15:45 +0300

    krivoy footer dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-const wiredep = require('gulp-wiredep')
+const wiredep = require('gulp-wiredep');
+var useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-clean-css');
  
+gulp.task('html', function () {
+    return gulp.src('*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('bower', function () {
   gulp.src('./index.html')
     .pipe(wiredep({
@@ -13,6 +25,30 @@ gulp.task('bower', function () {
 });
 
 
+gulp.task('images',function(){
+	gulp.src('img/**/*.jpg')
+		.pipe(gulp.dest('dist/img'));
+	gulp.src('img/**/*.png')
+		.pipe(gulp.dest('dist/img'));
+	gulp.src('img/**/*.svg')
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('fonts',function(){
+	gulp.src('fonts/**/*.eot)')
+		.pipe(gulp.dest('dist/fonts'));
+	gulp.src('fonts/**/*.ttf)')
+		.pipe(gulp.dest('dist/fonts'));
+	gulp.src('fonts/**/*.woff)')
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('build', [
+    'html',
+    'images',
+    'fonts'
+    
+]);
 gulp.task('sass', function () {
   return gulp.src('./templates/**/*.scss')
     .pipe(sass().on('error', sass.logError))
